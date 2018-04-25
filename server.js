b86740e896dc9ebd3cd96fede5e8fa345da034bf
joyce <joycedelatorre87@gmail.com>
2018-04-24 21:51:19 -0500

    set up server.js and added directories for views

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require ("express");
+var bodyParser = require("body-parser");
+var app = express();
+var port = 3000;
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+var exphbs = require("express.handlebars");
+app.engine("handlebars", exphbs({defaultLayout:"main"}));
+
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+	host:"localhost",
+	user:"root",
+	password:"toor",
+	database:"wishes_db"
+});
+
+connection.connect(function(err){
+	if(err){
+		console.error("error conncecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
+
+//--> here we will call the routes
+
+app.listen(port, function(){
+	console.log("SERVER STARTED");
+})
